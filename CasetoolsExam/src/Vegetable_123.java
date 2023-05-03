4e31ddf6cbfdbab9b91ee74fd9b14726a715dfb9
exam <exam@Bcalab4-m01>
2023-05-03 12:28:30 +0530

    First commit

---
+++ CasetoolsExam/src/Vegetable_123.java
@@ -0,0 +1,21 @@
+
+public class Vegetable_123 {
+	
+	String Color;
+	String name;
+	
+	Vegetable_123(String color)
+	{
+		this.Color=Color;
+	}
+	  void name(String Color)
+	{
+		this.Color=Color;	
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
